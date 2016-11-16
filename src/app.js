615855f13c41180b3185b914d075b6ed1202b111
Hoai Nam Duc Tran <kristjantran@yahoo.com>
2016-11-16 13:32:21 +0000

    a lot of shit

--- src/app.js
+++ src/app.js
@@ -3,35 +3,71 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const pgp = require('pg-promise')();
+const session = require('express-session');
 const router = express.Router();
 const user = require('./user.js');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname));
 app.set('view engine', 'pug');
+// breyta þarf hér fyrir    user   password                    database
+const db = pgp('postgres://postgres:anmineverdie94@localhost:5432/santaintrouble');
+app.use(session({
+  secret: 'jkhefhjNHmhgemjh7623ghw872jhgjHu72gh',
+  resave: false,
+  saveUninitialized: true
+}));
+
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'SantaInTroubles' });
+  console.log('yoyo');
+  res.render('index', { title: 'SantaInTroubles' , loggedIn: false});
 });
+
 
 app.post('/signup', (req, res) => {
   const username = req.body.userFieldInput;
   const password = user.checkValidPassword(req.body.passFieldInput);
   console.log("username : " + username);
   console.log("password : " + password);
+  db.any('insert into "user" (username, password) values ($1, $2)', [username, password]);
   res.render('index', { username, password });
 });
 
-// breyta þarf hér fyrir    user   password                    database
-const db = pgp('postgres://Notandi:qdsUf7ri@localhost:5432/santaintrouble');
+app.post('/login', (req, res) => {
+  const username = req.body.userFieldInput;
+  const password = user.checkValidPassword(req.body.passFieldInput);
+  db.any('select * from "user" where "user".username=$1 and "user".password=$2', [username, password])
+    .then((data) => {
+      req.session.username = username;
+      console.log(req.session.username);
+      res.redirect('/');
+    })
+    .catch((error) => {
+      console.log(('error', error));
+    });
+});
+
+function isLoggedIn(req, res, next) {
+  if(req.session.username) {
+    console.log('logged in');
+    next();
+  } else {
+    console.log('not logged in');
+    next();
+  }
+};
+
+
 app.get('/sql', (req, res, next) => {
-  db.any('select * from user')
+  db.any('select * from "user"')
     .then((data) => {
       for(var prop in data) {
         if(data.hasOwnProperty(prop)) {
           console.log(data);
-          console.log('data: ' + data[prop].username);
+          console.log('data: ' + data[prop]);
         }
       }
       res.render('data', { title: 'Data', data });
